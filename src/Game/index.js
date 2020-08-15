75974a03f7832f1a25fb94e55186e988ecdfef8c
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-08-15 11:20:28 -0300

    working on gesture handler

--- src/Game/index.js
+++ src/Game/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+import { PanGestureHandler } from 'react-native-gesture-handler';
 
 import styles from './styles';
 
 export default function Game() {
+    let [text, setText] = useState('vazio');
+
+    function controls(event) {
+    console.log(event.nativeEvent);
+        if (event.translationX > 50) {
+            setText('left to right');
+        } else if (event.translationX < -50) {
+            setText('right to left');
+        }
+
+        if (event.translationY > 50) {
+            setText('top');
+        } else if (event.translationY < -50) {
+            setText('bottom');
+        }
+    }
+
     return(
-        <Text style={styles.game}>over</Text>
+        <PanGestureHandler onGestureEvent={e => controls(e)}>
+            <View style={styles.game}><Text>{text}</Text></View>
+        </PanGestureHandler>
     )
 };
